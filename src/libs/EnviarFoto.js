d4655f471d11a6e8c2119c56b65a530f938baab3
T8ninho <tonisantos.contato@gmail.com>
2024-05-31 20:16:40 -0300

    ❤ Atualizado ordem de exibição das imagens pela data de envio

--- src/libs/EnviarFoto.js
+++ src/libs/EnviarFoto.js
@@ -1,7 +1,6 @@
 import { storage } from '../libs/firebase';
-import { ref, listAll, getDownloadURL, uploadBytes } from 'firebase/storage';
+import { ref, listAll, getDownloadURL, uploadBytes, getMetadata } from 'firebase/storage';
 import { v4 as createID } from 'uuid';
-
 
 const BancoImage = "Imagens";
 
@@ -11,24 +10,33 @@ export const getAll = async () => {
     const imagesFolder = ref(storage, BancoImage);
     const photoList = await listAll(imagesFolder);
 
-    for(let uniItem in photoList.items) {
-        let photoURL = await getDownloadURL(photoList.items[uniItem]);
+    for (let uniItem in photoList.items) {
+        let photoRef = photoList.items[uniItem];
+        let photoURL = await getDownloadURL(photoRef);
+        
+        // Obtendo os metadados da imagem
+        let metadata = await getMetadata(photoRef);
+        let uploadDate = new Date(metadata.timeCreated);
 
         list.push({
-            name: photoList.items[uniItem].name,
-            url: photoURL
-        })
+            name: photoRef.name,
+            url: photoURL,
+            date: uploadDate  // Adicionando a data ao objeto
+        });
+
+        console.log('data aqui:', uploadDate);  // Exibindo a data no console
     }
+
+    // Ordenar a lista pela data (mais recentes primeiro)
+    list.sort((a, b) => b.date - a.date);
 
     return list;
 }
 
 export const insert = async (file) => {
-
-    if(['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/webp'].includes(file.type)) {
-
+    if (['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/webp'].includes(file.type)) {
         let randomName = createID();
-        let newFile = ref(storage, `${BancoImage}/${randomName}`)
+        let newFile = ref(storage, `${BancoImage}/${randomName}`);
 
         let upload = await uploadBytes(newFile, file);
         let photoURL = await getDownloadURL(upload.ref);
@@ -37,9 +45,7 @@ export const insert = async (file) => {
             name: upload.ref.name, 
             url: photoURL 
         };
-
     } else {
-        return new Error("Tipo de arquivo não permitido.")
+        return new Error("Tipo de arquivo não permitido.");
     }
 }
-
