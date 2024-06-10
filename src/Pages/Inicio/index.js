d9a811c048faf83c794ec2be99dff38ddec19be9
T8ninho <tonisantos.contato@gmail.com>
2024-06-09 22:38:23 -0300

    💥 Atualizando para versão 2.0.0

--- src/Pages/Inicio/index.js
+++ src/Pages/Inicio/index.js
@@ -2,27 +2,29 @@ import React, { useState, useEffect } from "react";
 import Appbar from "../../Components/AppBar";
 import Footer from "../../Components/Footer";
 import PhotoItem from "../../Components/PhotoItem";
-import * as Photos from '../../libs/EnviarFoto';
+import {EnviarFoto} from '../../libs/EnviarFoto';
 import * as C from './index.style';
-import ImageCompressor from 'image-compressor.js';
-import { useRef } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { BancoImage, db } from "../../libs/firebase";
 
 export default function Inicio() {
-    const [uploading, setUploading] = useState(false);
     const [loading, setLoading] = useState(false);
     const [photos, setPhotos] = useState([]);
     const [ImageATV, setImageATV] = useState(null);
     const [ModalOpen, setModalOpen] = useState(false);
-    const [imagePreview, setImagePreview] = useState(null);
-    const fileInputRef = useRef(null);
+
 
     useEffect(() => {
-        const getPhotos = async () => {
+        const ReceberImagens = async () => {
             setLoading(true);
-            setPhotos(await Photos.getAll());
+            const querySnapshot = await getDocs(collection(db, BancoImage));
+            const ImagemList = querySnapshot.docs.map(doc => ({ id: doc.key, ...doc.data() }));
+            
+            setPhotos(ImagemList.sort((a, b) => b.date - a.date));
             setLoading(false);
-        }
-        getPhotos();
+        };
+        ReceberImagens();
+        
     }, []);
 
     useEffect(() => {
@@ -38,71 +40,16 @@ export default function Inicio() {
         };
     }, [ModalOpen]);
 
-    const handleImageChange = (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setImagePreview(reader.result);
-            };
-            reader.readAsDataURL(file);
-        } else {
-            setImagePreview(null);
-        }
-    };
-    const handleFormSubmit = async (e) => {
-        e.preventDefault();
-
-        const formData = new FormData(e.currentTarget);
-        const file = formData.get('image');
-
-        if (file && file.size > 0) {
-            setUploading(true);
-            // Comprimir a imagem antes de enviar
-            const compressedImage = await compressImage(file);
-            let result = await Photos.insert(compressedImage);
-            setUploading(false);
-            setImagePreview(null);
-            if (fileInputRef.current) {
-                fileInputRef.current.value = '';
-            }
-
-            if (result instanceof Error) {
-                alert(`${result.name} - ${result.message}`);
-            } else {
-                let newPhotoList = [...photos];
-                newPhotoList.push(result);
-                setPhotos(newPhotoList);
-            }
-        }
-    }
-
-    const handleFormReset = () => {
-        setImagePreview(null);
-        if (fileInputRef.current) {
-            fileInputRef.current.value = '';
-        }
-    };
-
-    const compressImage = async (file) => {
-        const compressedImage = await new Promise((resolve, reject) => {
-            new ImageCompressor(file, {
-                quality: 0.5, // Defina a qualidade da imagem desejada aqui (0.1 a 1)
-                success(result) {
-                    resolve(result);
-                },
-                error(err) {
-                    reject(err);
-                },
-            });
-        });
-        return compressedImage;
-    };
+    
 
     function AbrirImagem(Imagem) {
         setImageATV(Imagem)
         setModalOpen(true)
     }
+
+    const handleUpload = (uploadedAudios) => {
+        setPhotos(prevAudios => [...prevAudios, ...uploadedAudios]);
+      };
 
     return (
         <C.Container>
@@ -119,34 +66,8 @@ export default function Inicio() {
                         </C.ModalBotaoClose>
                     </C.ModalAberta>
                 </C.ModalContainer>
-                <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
-                    <div>
-                        <C.SelecionarIMG htmlFor="file-upload" className="custom-file-upload">
-                            {imagePreview ? 'Trocar imagem' : 'Enviar'}
-                        </C.SelecionarIMG>
-                        {imagePreview && 
-                            <>
-                                <C.CancelarIMG type="button" onClick={handleFormReset} value="Cancelar" />
-                                <C.EnviarIMG type="submit" value="Enviar" />
-                            </>
-                        }
-                        <input 
-                            id="file-upload"
-                            type="file" 
-                            name="image" 
-                            onChange={handleImageChange}
-                            ref={fileInputRef}
-                            style={{ display: 'none' }}
-                        />
-                        
-                    </div>
-                    {uploading && "Enviando...😍"}
-                    {imagePreview && 
-                        <C.ImagePreviewContainer>
-                            <C.ImagePreview src={imagePreview} alt="Preview" />
-                        </C.ImagePreviewContainer>
-                    }
-                </C.UploadForm>
+                
+                <EnviarFoto onUpload={handleUpload}/>
 
                 {loading &&
                     <C.ScreenWarning>
--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -1,18 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
-  // Galeria VitorVilela***
-  // apiKey: "AIzaSyBrY1fRaxThXYYfXiCNP0FRkvQXkInqxEg",
-  // authDomain: "vrvitorvilela.firebaseapp.com",
-  // projectId: "vrvitorvilela",
-  // storageBucket: "vrvitorvilela.appspot.com",
-  // messagingSenderId: "610792541375",
-  // appId: "1:610792541375:web:7a18eb48472c42a70cca0a",
-  // measurementId: "G-2R70SM0Z4K"
-
-  //Galeria React***
   apiKey: "AIzaSyACf3R945C3q3IEOAmbPg2DFGEVrKNFKyA",
   authDomain: "galeriareact-ddfd6.firebaseapp.com",
   projectId: "galeriareact-ddfd6",
@@ -24,4 +14,9 @@ const firebaseConfig = {
 
   
 const firebaseApp = initializeApp(firebaseConfig);
-export const storage = getStorage(firebaseApp);
+const storage = getStorage(firebaseApp);
+const db = getFirestore(firebaseApp)
+
+const BancoImage = "ImagensPublic";
+
+export { storage, db, BancoImage };
